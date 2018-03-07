13731f66b544569d7574ed8a801740e448326f51
Carlos Lenon <code@carloslenon.com>
2018-03-07 08:05:11 -0600

    add test

--- test/db/models/_statesMdls.test.js
+++ test/db/models/_statesMdls.test.js
@@ -1,23 +1,34 @@
 import models from './../../../src/db/models';
+import states from './../../../src/db/models/states';
+import sequelize from 'sequelize';
 
 let newState = {
     uui: 'fd277342-9db5-423c-999d-867082263fb2',
-    name: 'DONE'
+    name: 'INACTIVE'
 };
 
 beforeAll(() => {
     models.sequelize.sync();
 });
 
-describe('#stateModel using sequelize', () => {
+describe('state model', () => {
     test('model state should be defined', () => {
-        expect(models.States).toBeDefined();
+        expect(states).toBeDefined();
     });
 
-    describe('Adding a new state', () => {        
-        test('It should add a new state record', () => {
+    describe('add', () => {        
+        test('It should add a new state and return its', () => {
             expect(newState).toBeDefined();
-            expect();
+            models.States.create(newState).then((newState) => {
+                expect(newState.uuid).toEqual(newState.name);
+            });
+
+            // models.sequelize.sync({
+            //     force: true,
+            //     logging: console.log
+            // }).then(() => {
+            //     return states.create(newState);
+            // });
         })
     });    
 });

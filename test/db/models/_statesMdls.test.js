f0ae7afe854744c7acd2365f3d0f59c4578ea2e8
Carlos Lenon <code@carloslenon.com>
2018-03-08 01:02:10 -0600

    add coverage test

--- test/db/models/_statesMdls.test.js
+++ test/db/models/_statesMdls.test.js
@@ -1,41 +1,32 @@
 import Models from './../../../src/db/models';
-import Sequelize from 'sequelize';
-
-const stateMock = {    
-    name: "IN PROGRESS"
-};
 
 beforeAll(() => {
     Models.sequelize.sync();
 });
 
-describe('state model', () => {
+describe('STATE MODEL', () => {
     describe('model', () => {
         test('It should be defined', () => {
-            expect(Models.States).toBeDefined();
+            expect(Models.States).toBeDefined();            
         });
 
-        test('It should has a uuid attribute', () => {
-
+        test('It should has an id attribute', () => {
+            expect(Models.States.attributes.id).toBeDefined();
+        });
+        
+        test('It should has an uuid attribute', () => {
+            expect(Models.States.attributes.uuid).toBeDefined();
+        });
+        
+        test('It should has a name attribute', () => {
+            expect(Models.States.attributes.name).toBeDefined();
         });
     });
 
-    describe('add', () => {
-        describe('types for state to send', () => {
-            describe('uuid', () => {
-                test('It should be defined', () => {
-                    expect(stateMock.uuid).toBeDefined();  
-                });
-                test('uuid should be a UUID type', () => {                
-                    expect(stateMock.uuid).toBe(Sequelize.UUID);
-                });
-            })
-        });
-        
-        test('It should add a new state and return its', () => {
-            expect(stateMock).toBeDefined();
-            Models.States.create(stateMock).then((stateCreated) => {
-                expect(stateCreated.name).toEqual(stateMock.name);
+    describe('displaying records', () => {
+        test('It should get the INACTIVE state', () => {            
+            Models.States.findById('4b687a7d-d541-454d-938c-e012e82ceb62').then((stateFetched) => {               
+                expect(stateFetched.name).toBe('INACTIVE');
             });
         })
     });

2f57d223c40b7b026acea040ed17658824a5acaa
clenondavis <dev@carloslenon.com>
2018-03-09 10:03:40 -0600

    merge changes

--- test/db/models/_statesMdls.test.js
+++ test/db/models/_statesMdls.test.js
@@ -2,51 +2,14 @@
 
 import StatesCls from './../../../src/service/class/state/stateCls';
 
-<<<<<<< HEAD
-// let stateCls = new StatesCls();
-
-// beforeEach(() => {
-//     const stateCls = new StatesCls();
-// });
-
 describe('STATE MODEL', () => {
     let stateCls = new StatesCls();
 
-=======
-describe('STATE MODEL', () => {
-    let stateCls = new StatesCls();
-
->>>>>>> 5219adc9e585c6921151f3ce69090d1cd736bdbf
     describe('model', () => {
 
         test('It should be defined', () => {            
             expect(stateCls).toBeDefined();            
         });
-<<<<<<< HEAD
-
-        test('It should has an id attribute', () => {            
-            expect(stateCls.id).toBeDefined();
-        });
-        
-        test('It should has an uuid attribute', () => {            
-            expect(stateCls.uuid).toBeDefined();
-        });
-        
-        test('It should has a name attribute', () => {
-            expect(stateCls.name).toBeDefined();
-        });
-    });
-
-    describe('displaying records', () => {        
-        test('It should get a state record by "4b687a7d-d541-454d-938c-e012e82ceb62" uuid', () => {            
-            // Models.States.findById('4b687a7d-d541-454d-938c-e012e82ceb62').then((stateFetched) => {               
-            //     expect(stateFetched.name).toBe('INACTIVE');
-            // });
-
-            stateCls.fetchStatesById('4b687a7d-d541-454d-938c-e012e82ceb62').then((stateFetched) => {
-                expect(stateFetched.name).toBe('INACTIVE');
-            });
-=======
 
         test('It should has an id attribute', () => {            
             expect(stateCls.id).toBeDefined();
@@ -71,7 +34,6 @@ describe('STATE MODEL', () => {
             return stateCls.findById('4b687a7d-d541-454d-938c-e012e82ceb62').then((stateFetched) => {               
                 expect(stateCls.name).toBe('INACTIVE');
             });           
->>>>>>> 5219adc9e585c6921151f3ce69090d1cd736bdbf
         })
     });
 });
